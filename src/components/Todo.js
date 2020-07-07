b1cf1f48bd8d35acf792c372213ba38aab0ba130
jidelson <58210030+jidelson@users.noreply.github.com>
2020-07-07 00:10:08 -0700

    met mvp

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todo = props => {
+    console.log(props)
+    return (
+        <div
+        onClick={() => props.toggleTodo(props.todo.id)}
+        className={`todo${props.todo.completed ? "completed" : ""}`}
+        >
+            <p>{props.todo.task}</p>
+        </div>
+    )
+}
+
+export default Todo;
